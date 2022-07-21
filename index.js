8169f78aaa37eaea7e39110e998fd9901e72426b
kenenna-emelife <kenennaemelife@gmail.com>
2022-07-21 14:55:10 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("hello")
+let name = "derek"
+console.log(name)
+let kenenna = "chidera" + " " + "derek"
+console.log(kenenna)
+let word = 2 + "chidera"
+console.log(word)
+let x = 2346
+let y = 2345
+let division = x / y
+let multiplication = x * y
+let subtract = x - y
+let add = x + y
+console.log(division)
+console.log(add)
+console.log(subtract)
+console.log(multiplication)
+let w = 2
+let d = 3
+let equals = w != d
+console.log(equals)
